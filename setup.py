a2b2b15c37a05dad7b923e35ee7543f8a1e593fa
Mateusz <matek2001@onet.pl>
2022-09-04 21:20:47 +0200

    Deleted unnecessary files

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name='PasswordWallet',
-    version='1.0',
-    packages=find_packages(','),
-    author='Mateusz')
--- test.py
+++
@@ -1,6 +0,0 @@
-from models import Model
-
-m = Model()
-m.create_db()
-m.add_password('ja', 'da', 'rfs')
-print(m.show_passwords[0])
